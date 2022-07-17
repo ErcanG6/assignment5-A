cd8e04aee8df51791668fed8391bd0d3fd1e36cb
ErcanG6 <gokceke@oregonstate.edu>
2022-07-16 20:40:33 -0700

    committed compiles

--- file_sum.py
+++ file_sum.py
@@ -1,8 +1,8 @@
-def file_sum(sums):
+def file_sum(filename):
     total = 0
-    with open(nums,'r') as my_file:
+    with open(filename,"r") as my_file:
         for line in my_file:
-            numbers=line.split(',')
+            numbers=line.split()
             for num in numbers:
                 total +=float(num)
     with open('sums','w') as outfile:
@@ -10,3 +10,5 @@ def file_sum(sums):
     my_file.close()
     outfile.close()
     return total
+
+file_sum("nums")
